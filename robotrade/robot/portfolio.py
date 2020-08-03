efc6072834f4507a9a6e3ae0d0ab59384eed6a3d
Vitaly Sem <virtualik@gmail.com>
2020-08-04 01:57:12 +0300

    add_position method added

--- robotrade/robot/portfolio.py
+++ robotrade/robot/portfolio.py
@@ -15,3 +15,13 @@ class Portfolio():
         self.risk_tolerance = 0.0
         self.account_number = account_number
 
+    def add_position(self, symbol: str, asset_type: str, purchase_date: Optional[str], quantity: int = 0, purchase_price: float = 0.0) -> dict:
+
+        self.positions[symbol] = {}
+        self.positions[symbol]['symbol'] = symbol
+        self.positions[symbol]['quantity'] = quantity
+        self.positions[symbol]['purchase_price'] = purchase_price
+        self.positions[symbol]['purchase_date'] = purchase_date
+        self.positions[symbol]['asset_type'] = asset_type
+
+        return self.positions
