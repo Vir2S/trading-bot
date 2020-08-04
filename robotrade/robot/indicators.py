314711bd80e19522cffb996965755d4aec49a170
Vitaly Sem <virtualik@gmail.com>
2020-08-05 02:59:04 +0300

    change_in_price method added

--- robotrade/robot/indicators.py
+++ robotrade/robot/indicators.py
@@ -46,3 +46,16 @@ class Indicators():
 
         self._frame = price_data_frame
 
+    def change_in_price(self) -> pd.DataFrame:
+
+        locals_data = locals()
+        del locals_data['self']
+
+        column_name = 'change_in_price'
+        self._current_indicators[column_name] = {}
+        self._current_indicators[column_name]['args'] = locals_data
+        self._current_indicators[column_name]['func'] = self.change_in_price
+
+        self._frame[column_name] = self._price_groups['close'].transform(
+            lambda x: x.diff()
+        )
